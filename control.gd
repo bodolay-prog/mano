4cf3de650552156a151a571c00711ca3f7a6c177
João Pedro <joao.lima9221@gmail.com>
2025-10-18 11:56:36 -0300

    feat: Adicionando a tela de menu

---
+++ control.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on__x_1_pressed() -> void:
+	get_tree() .change_scene_to_file("")
+
+
+func _on__x_ia_pressed() -> void:
+	get_tree() .change_scene_to_file("")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
